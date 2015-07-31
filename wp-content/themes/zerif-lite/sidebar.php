7964c5849c27590057fb14adae5e25a67167a931
cbellom <cbellom@unal.edu.co>
2015-07-31 16:25:41 -0500

    Se cambia tema de content-single. se cambia estilo de side-bar

--- wp-content/themes/zerif-lite/sidebar.php
+++ wp-content/themes/zerif-lite/sidebar.php
@@ -6,27 +6,5 @@
  */
 ?>
 	<div id="secondary" class="widget-area" role="complementary">
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
-			<aside id="search" class="widget widget_search">
-				<?php get_search_form(); ?>
-			</aside>
-
-			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'zerif-lite' ); ?></h1>
-				<ul>
-					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-				</ul>
-			</aside>
-
-			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', 'zerif-lite' ); ?></h1>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</aside>
-
-		<?php endif; // end sidebar widget area ?>
-	</div><!-- #secondary -->	<?php		echo "<p id=search-2 class=widget widget_search>";		if(function_exists('webonary_searchform')) { webonary_searchform(); }		echo "</p>";				//if(is_page() && is_active_sidebar('sidebar-pages')) : dynamic_sidebar('sidebar-pages');		if(is_active_sidebar('sidebar-pages')) : dynamic_sidebar('sidebar-pages');		//elseif(is_active_sidebar('sidebar-blog')) : dynamic_sidebar('sidebar-blog');		endif; ?>
+			<?php		echo "<p id=search-2 class=widget widget_search>";		if(function_exists('webonary_searchform')) { webonary_searchform(); }		echo "</p>";				//if(is_page() && is_active_sidebar('sidebar-pages')) : dynamic_sidebar('sidebar-pages');		if(is_active_sidebar('sidebar-pages')) : dynamic_sidebar('sidebar-pages');		//elseif(is_active_sidebar('sidebar-blog')) : dynamic_sidebar('sidebar-blog');		endif; 				?>
+	</div><!-- #secondary -->
